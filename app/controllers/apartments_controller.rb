8e4ba2fbfcee9472d08aeaa3126ac803e2bffdec
Isabel <isanpe@gmail.com>
2022-07-22 02:48:17 -0400

    agrega formulario

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -2,4 +2,9 @@ class ApartmentsController < ApplicationController
     def index
         @apartments = Apartment.all
     end
+
+    def new
+        @apartment = Apartment.new
+        @building = Building.new
+    end
 end
