036f6687a396341be9267923ecbccd4dbdc574ba
Isabel <isanpe@gmail.com>
2022-07-24 02:59:32 -0400

    Cuarto commit

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -6,5 +6,29 @@ class ApartmentsController < ApplicationController
     def new
         @apartment = Apartment.new
         @building = Building.new
+        @building = Building.pluck(:name, :id)
     end
+
+    def show
+        @apartment = Apartment.find(params[:id])
+    end
+
+    def create
+        @apartment = Apartment.new(apartment_params)
+        
+        respond_to do |format| 
+            if @apartment.save
+                format.html { redirect_to @apartment, notice: 'Apartment was successfully created.'}
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+    private
+
+    def apartment_params
+        params.require(:apartment).permit(:number, :building_id)
+    end
+
 end
